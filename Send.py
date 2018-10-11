948e0dbe989beda2f3ae1f4f38134106d0666df4
VictorLMoura <victorlaperutademoura@hotmail.com>
2018-10-11 14:16:07 -0300

    envio

--- Send.py
+++ Send.py
@@ -5,23 +5,26 @@ import datetime
 
 def main():
 
-    SAMPLE_LEN = 44100 * 2 # 300 seconds of noise, 5 minutes
+    SAMPLE_LEN = 44100 * 2 # 2   seconds of noise, 5 minutes
 
     noise_output = wave.open('noise.wav', 'w')
     noise_output.setparams((2, 2, 44100, 0, 'NONE', 'not compressed'))
 
-    values = []
+    d1 = datetime.datetime.now()
 
     for i in range(0, SAMPLE_LEN):
         value = random.randint(-32767, 32767)
         packed_value = struct.pack('h', value)
-        values.append(packed_value)
-        values.append(packed_value)
+        noise_output.writeframes(packed_value)
+        noise_output.writeframes(packed_value)
 
-    value_str = ''.join(values)
-    noise_output.writeframes(value_str)
+    d2 = datetime.datetime.now()
+    print (d2 - d1), "(time for writing frames)"
 
     noise_output.close()
+
+    d3 = datetime.datetime.now()
+    print (d3 - d2), "(time for closing the file)"
     
 if __name__ == "__main__":
     main()
--- signalTeste.py
+++ signalTeste.py
@@ -31,23 +31,3 @@ class signalMeu:
         plt.figure()
         plt.plot(x, np.abs(y))
         plt.title('Fourier')
-
-
-if __name__ == "__main__":
-
-    noise_output = wave.open('noise.wav', 'w')
-    noise_output.setparams((2, 2, 44100, 0, 'NONE', 'not compressed'))
-
-    values = []
-
-    for i in range(0, SAMPLE_LEN):
-        value = random.randint(-32767, 32767)
-        packed_value = struct.pack('h', value)
-        values.append(packed_value)
-        values.append(packed_value)
-
-    value_str = ''.join(values)
-    noise_output.writeframes(value_str)
-
-    noise_output.close()
-
