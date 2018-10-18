cf8c262e64ce96b1494a8b8563999586a8ed215c
VictorLMoura <victorlaperutademoura@hotmail.com>
2018-10-18 16:53:11 -0300

    signal receiver

--- receive.py
+++ receive.py
@@ -8,10 +8,14 @@ import click
 import pickle
 import peakutils
 
+class receiver:
+    def __init__(self):
+        self.init = 0
 
-def main():
-    self.sm = signalMeu()
-    self.fs = 44100
+        self.sm = signalMeu()
+        self.frequencia = 44100
+        self.recorded = None
+
         user_freq = [697.0, 770.0, 852.0, 941.0,
                     1209.0, 1336.0, 1477.0, 1633.0]
         user_tones = {
@@ -33,48 +37,56 @@ def main():
             'D': (user_freq[3], user_freq[7]),
         }
 
-    self.matrix_frequencias =  {"1":(697,1209), "2":(697,1336), "3":(697,1209), "4":(770,1209), "5":(770,1336), "6":(770,1477), "7":(852,1209), "8":(852,1336), "9":(852,1477), "*":(941,1209), "0":(941,1336), "#":(941,1477)}
-    self.time = 0
+        self.matrix_frequencias =  {"1":(697,1209), "2":(697,1336), "3":(697,1209), "4":(770,1209), "5":(770,1336), "6":(770,1477), "7":(852,1209), "8":(852,1336), "9":(852,1477), "*":(941,1209), "0":(941,1336), "#":(941,1477)}
+        self.time = 0
 
-def record():
-    duration = 2
-    recording = sd.rec(int(duration * self.frequencia), samplerate = self.frequencia, channels=1)
-    sd.wait()
-    return recording[:,0]
 
-def findPeaks(self):
-    recording_fft = self.sig.calcFFT(self.record(), self.frequencia)
-    indexes = peakutils.indexes(recording_fft[1], thres=0.5, min_dist=10)
-    return indexes
+    def record(self):
+        duration = 2
+        recording = sd.rec(int(duration * self.frequencia), samplerate = self.frequencia, channels=1)
+        sd.wait()
+        self.recorded = recording[:,0]
+        return recording[:,0]
 
-def plotFFT2(self):
-    recording_fft = self.sig.calcFFT(self.record(), self.frequencia)
-    plt.figure()
-    plt.plot(recording_fft[0], np.abs(recording_fft[1]))
-    plt.title("Fourier")
-    plt.show()
+    def findPeaks(self):
+        recording_fft = self.sm.calcFFT(self.record(), self.frequencia)
+        indexes = peakutils.indexes(recording_fft[1], thres=0.5, min_dist=10)
+        return indexes
 
-def findKeys(self):
-    peaks = self.findPeaks()
-    possible_frequency = [697, 1209, 1336, 770, 852, 1477, 941]
-
-    received_frequency = []
-    for peak in peaks:
-        for frequency in possible_frequency:
-            if abs(index - frequency) < 50:
-                received_frequency.append(frequency)
-
-        #for key in self.matrix_frequencias.keys():
-        #    if
-
-def plotHarmonics():
-    frequencies = self.findPeaks()
-    for frequency in frequencies:
-        senoid = self.sig.generateSin(frequency, 1, 2, self.frequencia)
+    def plotFFT2(self):
+        recording_fft = self.sm.calcFFT(self.record, self.frequencia)
+        #recording_fft = self.sig.calcFFT(self.record(), self.frequencia)
         plt.figure()
-        plt.title("Gráfico para o harmonico de {} Hz".format(frequency))
-        plt.plot(senoid[0][:2000], senoid[1][:2000])
-    plt.show()
+        plt.plot(recording_fft[0], np.abs(recording_fft[1]))
+        plt.title("Fourier")
+        plt.show()
+
+    def findKeys(self):
+        peaks = self.findPeaks()
+        possible_frequency = [697, 1209, 1336, 770, 852, 1477, 941]
+
+        received_frequency = []
+        for peak in peaks:
+            for frequency in possible_frequency:
+                if abs(peak - frequency) < 50:
+                    received_frequency.append(frequency)
+
+            #for key in self.matrix_frequencias.keys():
+            #    if
+
+    def plotHarmonics(self):
+        frequencies = self.findPeaks()
+        for frequency in frequencies:
+            senoid = self.sm.generateSin(frequency, 1, 2, self.frequencia)
+            plt.figure()
+            plt.title("Gráfico para o harmonico de {} Hz".format(frequency))
+            plt.plot(senoid[0][:2000], senoid[1][:2000])
+        plt.show()
+
+    def main(self):
+        self.plotFFT2()
+        self.plotHarmonics()
 
 if __name__ == "__main__":
-    main()
+    rs = receiver()
+    rs.main()
